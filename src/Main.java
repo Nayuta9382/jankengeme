db5dfe936179f53a855c12eb702ab61881678979
Nayuta <n.kumagai.sys23@morijyobi.ac.jp>
2024-07-08 11:34:18 +0900

    PlayerクラスとMainクラスを実装した。　close #7,#8,#9

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか、
+// ガターで <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
+public class Main {
+    public static void main(String[] args) {
+        Player user = new Player("ユーザ");
+
+        Player com = new Player("CPU");
+
+
+        //入力指示表示
+
+        System.out.println("1.グー");
+
+        System.out.println("2.チョキ");
+
+        System.out.println("3.パー");
+
+        System.out.println("あなたの手を選択してください。>");
+    }
+}
